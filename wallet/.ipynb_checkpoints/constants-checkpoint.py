f7990448517264e2839c42eed6c1906482847da1
liberadori <pedroliberadori@gmail.com>
2020-05-29 19:23:44 -0400

    Stuck Halfway Through

---
+++ wallet/.ipynb_checkpoints/constants-checkpoint.py
@@ -0,0 +1,7 @@
+import os
+
+BTC = 'btc'
+ETH = 'eth'
+BTCTEST = 'btc-test'
+
+mnemonic = os.getenv('MNEMONIC','reunion priority ring open anxiety echo pigeon stumble wagon nest frog orbit inform add rescue')
