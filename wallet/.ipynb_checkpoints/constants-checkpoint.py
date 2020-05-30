4b40a7703c1766d8f2ae2234e74190f3311a0873
liberadori <pedroliberadori@gmail.com>
2020-05-30 17:52:23 -0400

    Final Commit

--- wallet/.ipynb_checkpoints/constants-checkpoint.py
+++ wallet/.ipynb_checkpoints/constants-checkpoint.py
@@ -1,7 +1,3 @@
-import os
-
 BTC = 'btc'
 ETH = 'eth'
 BTCTEST = 'btc-test'
-
-mnemonic = os.getenv('MNEMONIC','reunion priority ring open anxiety echo pigeon stumble wagon nest frog orbit inform add rescue')
